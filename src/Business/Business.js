1fac014677e7d4ec5eafa745b0fa68bb0fb11816
Pete <petemccarthy@users.noreply.github.com>
2018-05-02 22:18:56 -0700

    Finished Business Component

---
+++ src/Business/Business.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Business.css';
+
+const Business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
+
+class Business extends React.Component {
+    render() {
+
+        return (
+  <div className={Business.name}>
+  <div className="image-container">
+    <img src={Business.imageSrc} alt={Business.name}/>
+  </div>
+  <h2>MarginOtto Pizzeria</h2>
+  <div className="Business-information">
+    <div className="Business-address">
+      <p>{Business.address}</p>
+      <p>{Business.city}</p>
+      <p>{Business.state} {Business.zipCode}</p>
+    </div>
+    <div className="Business-reviews">
+      <h3>{Business.category}</h3>
+      <h3 className="rating">{Business.rating} stars}</h3>
+      <p>{Business.reviewCount} reviews</p>
+    </div>
+  </div>
+</div>
+        );
+    }
+}
+
+export default Business;
