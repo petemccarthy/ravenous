16723a6b158a2c174d5f35201d84bb8e50d77285
Pete <petemccarthy@users.noreply.github.com>
2018-05-18 19:10:54 -0700

    Last Changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+
+
 class App extends Component {
+
   render() {
+    const businesses = [
+      business, business, business, business, business, business
+    ];
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList business={businesses}/>
       </div>
     );
   }
